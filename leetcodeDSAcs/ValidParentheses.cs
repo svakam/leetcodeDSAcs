1ca9bfa374816f3e4ba33784ea869816d0567582
svakam <vikramakam@hotmail.com>
2020-09-24 18:34:31 -0700

    proj setup, valid parentheses

---
+++ leetcodeDSAcs/ValidParentheses.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcodeDSAcs
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            // instantiate stack class
+            Stack stack = new Stack();
+
+            // instantiate dictionary of related open/close brackets
+            Dictionary<char, char> openBrackets = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '{', '}' },
+                { '[', ']' }
+            };
+
+            Dictionary<char, char> closeBrackets = new Dictionary<char, char>
+            {
+                { ')', '(' },
+                { '}', '{' },
+                { ']', '[' }
+            };
+
+            // iterate through string
+            for (int i = 0; i < s.Length; i++)
+            {
+                // if open bracket is found, add to stack
+                if (openBrackets.ContainsKey(s[i])) stack.Add(s[i]);
+                // else if close bracket is found
+                else
+                {
+                    Node top = stack.Peek();
+                    // if top of stack is the opening of close bracket, remove top and move on, else validation failed and return false
+                    if (top != null && top.Value == closeBrackets[s[i]]) stack.Remove();
+                    else return false;
+                }
+            }
+
+            return stack.Peek() == null;
+        }
+    }
+
+    public class Stack
+    {
+        Node top;
+
+        public void Add(char c)
+        {
+            Node n = new Node
+            {
+                Value = c,
+                Next = null
+            };
+
+            if (top == null) top = n;
+            else
+            {
+                n.Next = top;
+                top = n;
+            }
+        }
+
+        public Node Remove()
+        {
+            if (top == null) return null;
+            Node temp = top;
+            if (top.Next == null)
+            {
+                top = null;
+                return temp;
+            }
+
+            temp = top;
+            top = top.Next;
+            temp.Next = null;
+            return temp;
+        }
+
+        public Node Peek()
+        {
+            return top;
+        }
+    }
+
+    public class Node
+    {
+        public char Value { get; set; }
+        public Node Next { get; set; }
+    }
+}
