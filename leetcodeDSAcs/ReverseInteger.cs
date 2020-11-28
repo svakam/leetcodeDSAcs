f2d54134c3eb0eeb5e53cc4e694b0a986804a06e
svakam <vikramakam@hotmail.com>
2020-11-28 12:38:07 -0800

    reverse integer

---
+++ leetcodeDSAcs/ReverseInteger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeDSAcs
+{
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            if (x > -10 && x < 10) return x;
+
+            // if negative, convert to positive for time being
+            bool isNegative = false;
+            if (x < 0)
+            {
+                isNegative = true;
+                x *= -1;
+            }
+
+            // get number of digits
+            int numDigits = (int)Math.Floor(Math.Log10(x) + 1);
+
+            // start counter for power incrementation
+            int power = numDigits - 1;
+            // start new integer
+            int newInt = 0;
+            // while input is not 0
+            while (x != 0)
+            {
+                // get end integer by modulo and add it to new integer via power incrementing
+                try
+                {
+                    checked
+                    {
+                        newInt = newInt + (x % 10) * (int)Math.Pow(10, power);
+                    }
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine(ex);
+                    return 0;
+                }
+                power--;
+                // remove end integer by division
+                x /= 10;
+            }
+
+            // add back negative if needed
+            if (isNegative)
+            {
+                newInt *= -1;
+            }
+
+            return newInt;
+        }
+    }
+}
---
+++ leetcodeDSAcs/RomanToInteger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeDSAcs
+{
+    public class RomanToInteger
+    {
+        public int RomanToInt(string s)
+        {
+            int final = 0;
+            Dictionary<char, int> symbolValues = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+            Dictionary<char, char[]> subtractions = new Dictionary<char, char[]>()
+            {
+                {'I', new char[]{'V', 'X' } },
+                {'X', new char[]{'L', 'C' } },
+                { 'C', new char[]{'D', 'M' } }
+            };
+            for (int i = 0; i<s.Length; i++)
+            {
+                if (subtractions.ContainsKey(s[i]))
+                {
+                    if ((i + 1) <= s.Length - 1)
+                    {
+                        for (int j = 0; j< 2; j++)
+                        {
+                            if (s[i + 1].Equals(subtractions[s[i]][j]))
+                            {
+                                int temp = symbolValues[s[i + 1]] - symbolValues[s[i]];
+                                final += temp;
+                            }
+                        }
+                        i++;
+                    }
+                }
+                else
+                {
+                    final += symbolValues[s[i]];
+                }
+            }
+        
+            return final;
+        }
+    }
+}
