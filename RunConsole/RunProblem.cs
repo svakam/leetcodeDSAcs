d58d22e0da7f04051b3b2e937499cf752f31657a
svakam <vikramakam@hotmail.com>
2021-02-14 23:37:28 -0800

    reconfigured console app, referenced library proj, setting up prompts

---
+++ RunConsole/RunProblem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using leetcodeDSAcs;
+
+namespace RunConsole
+{
+    class RunProblem
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leetcode problems!");
+            Console.WriteLine("Choose a problem to run by difficulty, data structure type, or directly by problem name: ");
+            Console.WriteLine("If by difficulty, type \"df\"; if by data structure, type \"ds\"; if by problem type, type \"p\". Then press Enter: ");
+            switch (Console.ReadLine())
+            {
+                case "df":
+                    ChooseByDifficulty(args);
+                    break;
+                case "ds":
+                    ChooseByDataStructure(args);
+                    break;
+                case "p":
+                    ChooseByProblem(args);
+                    break;
+                case "r":
+                    ChooseRandom(args);
+                    break;
+                default:
+                    Console.WriteLine("Try again. ");
+                    return;
+            }
+        }
+
+        public static void ChooseByDifficulty(string[] args)
+        {
+            Console.WriteLine("Choose by difficulty: Easy (e) or Medium (m)");
+            switch (Console.ReadLine())
+            {
+                case "e":
+                    Console.WriteLine("Choose a problem by data structure, problem name, or random problem: ");
+                    break;
+                case "m":
+                    Console.WriteLine("Choose a problem by data structure, problem name, or random problem: ");
+                    break;
+                default:
+                    Console.WriteLine("Try again. ");
+                    return;
+            }
+        }
+
+        public static void ChooseByDataStructure(string[] args)
+        {
+            Console.WriteLine("bye");
+        }
+
+        public static void ChooseByProblem(string[] args)
+        {
+            Console.WriteLine("oh no");
+        }
+
+        public static void ChooseRandom(string[] args)
+        {
+            Console.WriteLine("Random problem!");
+        }
+    }
+}
--- leetcodeDSAcs/Medium/ValidSubArrays.cs
+++ leetcodeDSAcs/Medium/ValidSubArrays.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace leetcodeDSAcs.Medium
-{
-    public class ValidSubArrays
-    {
-        public int NumSubarrayProductLessThanK(int[] nums, int k)
-        {
-            if (nums.Length < 1)
-            {
-                if (nums.Length == 0) return 0;
-                if (nums[0] < k) return k;
-                return 0;
-            }
-            if (k == 0) return 0;
-            return GetNumOfSubArrays(0, 0, nums.Length - 1, 1, 0, nums, k);
-            // 
-        }
+//namespace leetcodeDSAcs.Medium
+//{
+//    public class ValidSubArrays
+//    {
+//        public int NumSubarrayProductLessThanK(int[] nums, int k)
+//        {
+//            if (nums.Length < 1)
+//            {
+//                if (nums.Length == 0) return 0;
+//                if (nums[0] < k) return k;
+//                return 0;
+//            }
+//            if (k == 0) return 0;
+//            return GetNumOfSubArrays(0, 0, nums.Length - 1, 1, 0, nums, k);
+//            // 
+//        }
 
-        public int GetNumOfSubArrays(int current, int beginning, int end, int currentProduct, int numOfValidSubArrays, int[] nums, int k)
-        {
-            if (nums[current] < k)
-            {
-                numOfValidSubArrays++;
-                currentProduct *= nums[beginning];
-                if 
-            }
-            GetNumOfSubArrays
-        }
-    }
-}
+//        public int GetNumOfSubArrays(int current, int beginning, int end, int currentProduct, int numOfValidSubArrays, int[] nums, int k)
+//        {
+//            if (nums[current] < k)
+//            {
+//                numOfValidSubArrays++;
+//                currentProduct *= nums[beginning];
+//                if 
+//            }
+//            GetNumOfSubArrays
+//        }
+//    }
+//}
