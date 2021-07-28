78503ad6bbe7a1f0ce83af83fea8b936d17fa450
Vik A <vikramakam@hotmail.com>
2021-07-28 14:47:45 -0700

    implement strstr

---
+++ leetcodeDSAcs/Easy/Strings/ImplementStrStr.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeDSAcs.Easy.Strings
+{
+    class ImplementStrStr
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0) return 0;
+            if ((haystack.Length == 0 && needle.Length > 0) || (needle.Length > haystack.Length)) return -1;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    if (IsNeedleInHaystack(haystack, needle, i))
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        public bool IsNeedleInHaystack(string haystack, string needle, int i)
+        {
+            for (int j = 1; j < needle.Length; j++)
+            {
+                if (i + 1 == haystack.Length) return false;
+                if (needle[j] != haystack[i + 1])
+                {
+                    return false;
+                }
+                i++;
+            }
+
+            return true;
+        }
+    }
+}
