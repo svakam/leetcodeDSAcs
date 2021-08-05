a4eec6cb025b180c1f0274cfb067b461507dbec2
Vik A <vikramakam@hotmail.com>
2021-08-05 01:01:24 -0700

    strstr, concat arr

---
+++ leetcodeDSAcs/Easy/Arrays/ConcatenationOfArr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeDSAcs.Easy.Arrays
+{
+    // runtime: 240 ms, 94.78% faster
+    // memory: 33.7 mb, 55% less
+    class ConcatenationOfArr
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int length = nums.Length;
+            int[] ans = new int[length * 2];
+            bool flag = false;
+            int n = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                if (flag)
+                {
+                    n += length;
+                }
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    ans[j + n] = nums[j];
+                }
+                flag = true;
+            }
+
+            return ans;
+        }
+    }
+}
--- leetcodeDSAcs/Easy/Strings/ImplementStrStr.cs
+++ leetcodeDSAcs/Easy/Strings/ImplementStrStr.cs
@@ -6,6 +6,8 @@ namespace leetcodeDSAcs.Easy.Strings
 {
     class ImplementStrStr
     {
+        // runtime exceeds limit for very large strings
+        // memory: 37.59% better
         public int StrStr(string haystack, string needle)
         {
             if (needle.Length == 0) return 0;
@@ -38,6 +40,12 @@ namespace leetcodeDSAcs.Easy.Strings
             }
 
             return true;
+        }
+
+        public int StrStrAlternative(string haystack, string needle)
+        {
+            // https://leetcode.com/problems/implement-strstr/discuss/1066884/c-kmp
+            // https://leetcode.com/problems/implement-strstr/discuss/402189/C-Substring-Solution
         }
     }
 }
