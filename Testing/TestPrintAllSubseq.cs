cb02a470f120863aa120d98aea84e2c46fde0b1f
Vik Akam <vikramakam@hotmail.com>
2022-10-09 13:38:34 -0700

    added print subseq test but dysfunc

---
+++ Testing/TestPrintAllSubseq.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using leetcodeDSAcs.Easy.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class TestPrintAllSubseq
+    {
+        [TestMethod]
+        public void Test()
+        {
+            PrintAllSubseq soln = new();
+            soln.Run("ABCD");
+            LinkedList<string> results = soln.Subseq;
+            foreach (string s in results)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+    }
+}
---
+++ leetcodeDSAcs/Easy/Strings/PrintAllSubseq.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeDSAcs.Easy.Strings
+{
+    public class PrintAllSubseq
+    {
+        public LinkedList<string> Subseq { get; set; }
+        public PrintAllSubseq()
+        {
+            Subseq = new LinkedList<string>();
+        }
+
+        public void Run(string str, string subseq = "")
+        {
+            if (str.Length == 0)
+            {
+                Subseq.AddLast("\"" + subseq + "\"");
+                return;
+            }
+
+            Run(str.Substring(1, str.Length), subseq + str[0]); // include the char
+            Run(str.Substring(1, str.Length), subseq); // exclude the char
+        }
+    }
+}
+
+// print("ABCD", "")
+// print("BCD", "A") 1
+// print("CD", "AB") 1
+// print("D", "ABC") 1
+// print("", "ABCD") 1
+// -> ["ABCD"]
+// print("", "ABC") 2
+// -> ["ABCD", "ABC"]
+// print("", "AB") 1
+// -> ["ABCD", "ABC", "AB"]
+// print("", "A") 2
+// -> ["ABCD", "ABC", "AB", "A"]
