244b976cf3b18102703218580801d4fe6c19449a
Vik Akam <vikramakam@hotmail.com>
2022-10-10 16:08:20 -0700

    subsets of int set, wrote test but vs testtools is poopoo

---
+++ Testing/TestPrintSubsetsInt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using leetcodeDSAcs.Medium.Arrays;
+
+namespace Testing
+{
+    [TestClass]
+    public class TestPrintSubsetsInt
+    {
+        [TestMethod]
+        public void Test()
+        {
+            int[] arr = { 1, 2 };
+            Console.WriteLine(arr);
+            PrintSubsetsOfSet soln = new();
+            soln.Run(arr);
+            var results = soln.Output;
+            foreach (int[] array in results)
+            {
+                Console.WriteLine(array.ToString());
+            }
+
+            // expect:
+            // [null,null], [1,null], [1,2], [null,2]
+            //int[] arr2 = { 1, 2, 3 };
+            //soln.Run(arr2);
+            //results = soln.Output;
+            //foreach (int[] array in results)
+            //{
+            //    Console.WriteLine(array.ToString());
+            //}
+            // expect:
+            // [null,null,null],[1,null,null],[1,2,null],[1,2,3],[1,null,3],
+            // [null,2,null],[null,2,3],
+            // [null,null,3]
+        }
+    }
+}
---
+++ leetcodeDSAcs/Medium/Arrays/PrintSubsetsOfSet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeDSAcs.Medium.Arrays
+{
+    // number of subsets = 2^n, n = number of elements in input
+        // should include nth element in current subset or not?
+        // for each item arr[n], there are two available choices (to include or not to include), thus 2^n
+    public class PrintSubsetsOfSet
+    {
+        public LinkedList<int[]> Output { get; set; }
+        public PrintSubsetsOfSet()
+        {
+            Output = new LinkedList<int[]>();
+        }
+        public void Run(int[] arr)
+        {
+            int[] subset = new int[arr.Length];
+            Helper(arr, subset, 0);
+        }
+
+        private void Helper(int[] arr, int[] subset, int i)
+        {
+            Console.WriteLine(subset.ToString());
+            // base case: if i exceeds arr length
+            if (i == arr.Length)
+            {
+                Output.AddLast(subset);
+                return;
+            }
+            // recursion: include current elem or exclude
+            Helper(arr, subset, i++);
+            subset[i] = arr[i];
+            Helper(arr, subset, i++);
+        }
+    }
+}
+// [,,]
