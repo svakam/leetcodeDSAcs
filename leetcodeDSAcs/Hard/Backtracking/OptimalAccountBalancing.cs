82e13e060b5efe20581ae30c6dc024459362d68e
Vik Akam <vikramakam@hotmail.com>
2022-10-26 18:53:11 -0700

    added url parser, added sol'n optimal acc bal

--- leetcodeDSAcs/Hard/Backtracking/OptimalAccountBalancing.cs
+++ leetcodeDSAcs/Hard/Backtracking/OptimalAccountBalancing.cs
@@ -10,7 +10,14 @@ namespace leetcodeDSAcs.Hard.Backtracking
     {
         public int MinTransfers(int[][] transactions)
         {
-            
+            // build balances across ids via transactions
+            Dictionary<int, int> balances = new Dictionary<int, int>();
+            for (int i = 0; i < transactions.Length; i++)
+            {
+                
+            }
+
+            // reiterate 
         }
     }
 }
@@ -38,3 +45,53 @@ namespace leetcodeDSAcs.Hard.Backtracking
 // 3 = $2
 // 4 = $-1
 // 5 = $1
+
+//   public int minTransfers(int[][] transactions) {
+//int[] debt = buildDebtArray(transactions); // Debt amount to balance for each person.
+
+//return getMinTransfersAfter(0, debt);
+//    }
+    
+//    private int getMinTransfersAfter(int curId, int[] debt)
+//{
+//    while (curId < debt.length && debt[curId] == 0)
+//        curId++;
+//    // Base case.
+//    if (curId == debt.length)
+//        return 0;
+//    // Recursive case.
+//    int minTransactions = Integer.MAX_VALUE;
+//    for (int i = curId + 1; i < debt.length; i++)
+//    {
+//        if (debt[i] * debt[curId] < 0)
+//        {
+//            debt[i] += debt[curId];
+//            minTransactions = Math.min(minTransactions, getMinTransfersAfter(curId + 1, debt) + 1);
+//            debt[i] -= debt[curId];
+//        }
+//    }
+
+//    return minTransactions;
+//}
+
+//private int[] buildDebtArray(int[][] transactions)
+//{
+//    Map<Integer, Integer> debtMap = new HashMap<>(); // Map person ID to debt amount.
+
+//    for (int[] transaction : transactions) {
+//    int giver = transaction[0];
+//    int taker = transaction[1];
+//    int amount = transaction[2];
+//    debtMap.put(giver, debtMap.getOrDefault(giver, 0) + amount);
+//    debtMap.put(taker, debtMap.getOrDefault(taker, 0) - amount);
+//}
+
+//int[] debt = new int[debtMap.size()];
+//int i = 0;
+//for (int amount : debtMap.values())
+//{
+//    debt[i++] = amount;
+//}
+
+//return debt;
+//    }
---
+++ leetcodeDSAcs/Medium/Strings/urlParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcodeDSAcs.Medium.Strings
+{
+    public class QueryParser
+    {
+        public static object Run(string url)
+        {
+            object output = new object();
+            StringBuilder sb;
+
+            int i = 1;
+            while (i < url.Length)
+            {
+                sb = new StringBuilder();
+                object kv = ParseKv(url, sb, i);
+            }
+        }
+
+        private static object ParseKv(string url, StringBuilder sb, int i)
+        {
+            StringBuilder sbKey = new StringBuilder();
+            StringBuilder sbVal = new StringBuilder();
+            while (url[i] != '=')
+            {
+                sbKey.Append(url[i]);
+                ++i;
+            }
+            ++i; // skip over =
+            while (url[i] != '&')
+            {
+                sbVal.Append(url[i]);
+                ++i;
+            }
+            ++i; // skip over &
+            string key = sbKey.ToString();
+            string val = sbVal.ToString();
+            object key
+        }
+    }
+}
+
+// "?foo=one&bar=two&baz=three" -> 
+// {
+//  "foo": "one",
+//  "bar": "two",
+//  "baz": "three"
+// }
+// every input begins with "?"
